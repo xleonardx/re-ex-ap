a53a6cd57c495b996837b98a3417e2dd8ee24643
leibibi <avila.leonardxiii@gmail.com>
2020-08-22 14:26:29 +0000

    Updates

---
+++ precache-manifest.f14871a00b293c8ad537fbfb18ba923a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433a4f1144c8e907d6d38212d6413ade",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4293f77dbd3bd646e82",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "399d4ed313c6ebf3efdc",
+    "url": "/static/js/2.0815899e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0815899e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4293f77dbd3bd646e82",
+    "url": "/static/js/main.48dfd609.chunk.js"
+  },
+  {
+    "revision": "52005156d476b605d4d5",
+    "url": "/static/js/runtime-main.5a443804.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
